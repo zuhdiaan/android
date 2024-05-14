c2e1f93e68ee720d111fc7faddd27f58d6033e76
Zuhdi <farhanzuhdiaan@gmail.com>
2024-05-14 21:46:17 +0700

    homepage done

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
   import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import LoginScreen from './Apps/Screens/LoginScreen';
+import Homepage from './Apps/Screens/Homepage';
 
 export default function App() {
   return (
     <View className="flex-1 bg-white">
       {/* <Text className="text-[40px] text-red-400">Open up App.js to start working on your app!</Text> */}
       <StatusBar style="auto" />
-      <LoginScreen/>
+      <Homepage/>
     </View>
   );
 }
---
+++ Apps/Screens/Homepage.jsx
@@ -0,0 +1,27 @@
+import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { useNavigation } from "@react-navigation/native"
+import React from 'react'
+import Menu from './Menu'
+
+export default function Homepage() {
+  const navigation = useNavigation()
+  return (
+    <View className="bg-black">
+      <Image source={require("./../../assets/images/homepage.jpg")}
+        className="w-full h-[600px] object-cover"
+      />
+      <View className="p-10 bg-white mt-[-15px] rounded-t-3xl">
+        <Text className="text-[30px] font-bold">Jiwani</Text>
+        <Text className="text-[18px] text-slate-500 mt-2">Everyone Has Their Own Space</Text>
+        <TouchableOpacity onPress={() => {goToNextScreen()}}
+        className="p-3 bg-blue-500 rounded-full mt-20">
+            <Text className="text-white text-center text-[18px]">Get Started</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+const goToNextScreen = () => {
+  navigation.navigate(Menu)
+}
